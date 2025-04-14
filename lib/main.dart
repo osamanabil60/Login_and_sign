b80ba69f07df49a717b1e3b69beb97c2fde363c3
Osama Nabil <0samanabilessam@gmail.com>
2025-04-14 13:20:41 +0200

    login and page

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'Layout/login.dart';
+import 'dart:async';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LoginScreen(),
+  ));
+}
